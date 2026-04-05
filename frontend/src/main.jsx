30a38c0c418c37c4d57d8519d00cdaf3e4f78e83
shivanshu gupta <rajkishoregupta716@gmail.com>
2026-04-05 08:43:36 +0530

    change

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
-const stripePromise = loadStripe(
-  "pk_test_51TIRSMKQAYlaOC0T0lAYnxXmjQHMBc2nVARDhGGWbJlya6I9uTgvkqoJWSMMpS3CnWy9yzXb6WC8Iiz8pxXwnznU00Sap0KNjt"
-);
+
 
 createRoot(document.getElementById("root")).render(
   <Elements stripe={stripePromise}>
@@ -17,3 +13,11 @@ createRoot(document.getElementById("root")).render(
     </BrowserRouter>
   </Elements>
 );
+
+
+
+// 🔐 Stripe Promise using .env
+const stripePromise = loadStripe(
+  import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY
+);
+
